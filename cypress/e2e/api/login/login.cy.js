e69268ccc9e32b8454a1ee3d8bfd4969c8579527
Lucas Camargo <nascimentocamargolucas@gmail.com>
2026-02-17 15:24:07 -0300

    Developing documentation.

--- cypress/e2e/api/login/login.cy.js
+++ cypress/e2e/api/login/login.cy.js
@@ -1,16 +1,16 @@
 describe('API Login - ServeRest', () => {
 
-  const endpoint = '/login'
-  const usersEndpoint = '/usuarios'
+  const endpoint = '/login';
+  const usersEndpoint = '/usuarios';
 
-  let emailValid
-  let senhaValid
+  let emailValid;
+  let senhaValid;
 
   before(() => {
     // Criar usuário antes dos testes
-    const timestamp = Date.now()
-    emailValid = `qa_${timestamp}@test.com`
-    senhaValid = 'Teste123!'
+    const timestamp = Date.now();
+    emailValid = `qa_${timestamp}@test.com`;
+    senhaValid = 'Teste123!';
 
     cy.request({
       method: 'POST',
@@ -22,8 +22,8 @@ describe('API Login - ServeRest', () => {
         administrador: 'true'
       }
     }).then((response) => {
-      expect(response.status).to.eq(201)
-      expect(response.body.message).to.eq('Cadastro realizado com sucesso')
+      expect(response.status).to.eq(201);
+      expect(response.body.message).to.eq('Cadastro realizado com sucesso');
     })
   })
 
@@ -38,11 +38,11 @@ describe('API Login - ServeRest', () => {
           password: senhaValid
         }
       }).then((response) => {
-        expect(response.status).to.eq(200)
-        expect(response.body.message).to.eq('Login realizado com sucesso')
-        expect(response.body).to.have.property('authorization')
-        expect(response.body.authorization).to.be.a('string')
-        expect(response.body.authorization).to.contain('Bearer')
+        expect(response.status).to.eq(200);
+        expect(response.body.message).to.eq('Login realizado com sucesso');
+        expect(response.body).to.have.property('authorization');
+        expect(response.body.authorization).to.be.a('string');
+        expect(response.body.authorization).to.contain('Bearer');
       })
     })
 
@@ -60,8 +60,8 @@ describe('API Login - ServeRest', () => {
           password: senhaValid
         }
       }).then((response) => {
-        expect(response.status).to.eq(401)
-        expect(response.body.message).to.eq('Email e/ou senha inválidos')
+        expect(response.status).to.eq(401);
+        expect(response.body.message).to.eq('Email e/ou senha inválidos');
       })
     })
 
@@ -75,8 +75,8 @@ describe('API Login - ServeRest', () => {
           password: 'senhaErrada123'
         }
       }).then((response) => {
-        expect(response.status).to.eq(401)
-        expect(response.body.message).to.eq('Email e/ou senha inválidos')
+        expect(response.status).to.eq(401);
+        expect(response.body.message).to.eq('Email e/ou senha inválidos');
       })
     })
 
@@ -90,8 +90,8 @@ describe('API Login - ServeRest', () => {
           password: 'senhaErrada123'
         }
       }).then((response) => {
-        expect(response.status).to.eq(401)
-        expect(response.body.message).to.eq('Email e/ou senha inválidos')
+        expect(response.status).to.eq(401);
+        expect(response.body.message).to.eq('Email e/ou senha inválidos');
       })
     })
 
@@ -108,8 +108,8 @@ describe('API Login - ServeRest', () => {
           password: senhaValid
         }
       }).then((response) => {
-        expect(response.status).to.eq(400)
-        expect(response.body.email).to.eq('email é obrigatório')
+        expect(response.status).to.eq(400);
+        expect(response.body.email).to.eq('email é obrigatório');
       })
     })
 
@@ -122,8 +122,8 @@ describe('API Login - ServeRest', () => {
           email: emailValid
         }
       }).then((response) => {
-        expect(response.status).to.eq(400)
-        expect(response.body.password).to.eq('password é obrigatório')
+        expect(response.status).to.eq(400);
+        expect(response.body.password).to.eq('password é obrigatório');
       })
     })
 
@@ -134,9 +134,9 @@ describe('API Login - ServeRest', () => {
         failOnStatusCode: false,
         body: {}
       }).then((response) => {
-        expect(response.status).to.eq(400)
-        expect(response.body.email).to.eq('email é obrigatório')
-        expect(response.body.password).to.eq('password é obrigatório')
+        expect(response.status).to.eq(400);
+        expect(response.body.email).to.eq('email é obrigatório');
+        expect(response.body.password).to.eq('password é obrigatório');
       })
     })
 
