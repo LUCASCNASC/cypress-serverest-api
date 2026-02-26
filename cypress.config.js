c721c0e2674e5d239083a9fac6d2e4daaa6a8f2c
Lucas Camargo <nascimentocamargolucas@gmail.com>
2026-02-26 20:12:17 -0300

    Developing project.

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,17 @@
 const { defineConfig } = require("cypress");
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://serverest.dev",
+    // Puxa a URL do .env ou usa a oficial como fallback
+    baseUrl: process.env.BASE_URL || "https://serverest.dev",
     env: {
-      EMAIL_USER_VALID: "testelucas@qa.com",
-      SENHA_USER_VALID: "@LucasQA01"
-    }
-  }
-})
+      // Mapeia as variáveis do sistema/dotenv para o Cypress
+      EMAIL_USER_VALID: process.env.EMAIL,
+      SENHA_USER_VALID: process.env.PASSWORD,
+    },
+    setupNodeEvents(on, config) {
+      // Implemente node event listeners aqui se necessário
+    },
+  },
+});
